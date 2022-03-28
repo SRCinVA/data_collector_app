8d56774d5ecf4d1e1a001c7cce5b277e9b051a96
SRCinVA <scitarella@hotmail.com>
2022-03-27 20:52:34 -0400

    More for Flask

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app=Flask(__name__)
 @app.route("/")  # for our index page
 def index():
     return render_template("index.html")
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+if __name__ == '__main__': #meaning that this is script is being executed and NOT imported
+    app.debug=True
+    app.run()  #we'll run the application in the default port (5000)
