6b3d9322177d49fcb97af466c6672d3be606961c
SRCinVA <scitarella@hotmail.com>
2022-04-05 22:02:12 -0400

    Will continue to code, but can't get this to work

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/success")
+@app.route("/success", methods=['POST'])
 def success():
-    return render_template("success.html")
+    if request.method=='POST':
+        email=request.form["email_name"]
+        print(email)
+        return render_template("success.html")
 
-if __name__ == '__main__': #meaning that this is script is being executed and NOT imported
+if __name__ == "__main__": #meaning that this is script is being executed and NOT imported
     app.debug=True
     app.run()  #we'll run the application in the default port (5000)
